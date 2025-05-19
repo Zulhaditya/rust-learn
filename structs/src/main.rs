952add9360a409466e94fda22d6917eddc3b0556
Zulhaditya <zulhaditya@gmail.com>
2025-05-19 23:27:54 +0700

    UPDATE : define structs

---
+++ structs/src/main.rs
@@ -0,0 +1,34 @@
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        active: true,
+        username,
+        email,
+        sign_in_count: 1,
+    }
+}
+
+fn main() {
+    let mut user1 = User {
+        active: true,
+        username: String::from("Zulhaditya"),
+        email: String::from("zulhaditya@gmail.com"),
+        sign_in_count: 1,
+    };
+
+    user1.email = String::from("Inayah");
+    println!("{}", user1.email);
+
+    build_user(String::from("Ackxle"), String::from("ackxle@email.com"));
+
+    let user2 = User {
+        email: String::from("another@example.com"),
+        ..user1
+    };
+}
